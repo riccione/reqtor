d95338d60b71a61f70933516f4dc6cbe55e6dfbe
riccione <28346232+riccione@users.noreply.github.com>
2026-06-18 11:52:23 +0200

    chore: bump version to 0.3.0

--- src/reqtor/__init__.py
+++ src/reqtor/__init__.py
@@ -5,7 +5,7 @@ from reqtor.fixtures import api_fixture
 from reqtor.response import Response
 
 __all__ = ["API", "Response", "api_fixture"]
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 
 def __getattr__(name: str) -> object:
